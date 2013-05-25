9e3a6747385866211b9cca9dc02375c3ea6ed21c
miguel <miguel.garciarod@gmail.com>
2013-05-25 15:16:29 +0200

    Some changes in the organization

--- war/static/js/script.js
+++ war/static/js/script.js
@@ -8,71 +8,69 @@ $('div.start img.socketImg').click(readyToCharge);
 
 $('div.start img.carImg').click(constructingModalShow);
 
-    function loginUser(){
-      var user = $('form.login input.username').val();
+    function loginUser() {
+      var user = $('form.login input:eq(0)').val();
       var pass = $('form.login input[type="password"]').val();
       pass = $.md5(pass);
       //var form = $('form.login').serialize();
       //console.log(form);
-      if (user=="" || pass=="")
+      if (user === "" || pass === "") {
         $('div.modal.userError').modal("show");
-      else
-        {
-        $.ajax({
-            url: '/app/Users/' + user + '/' + pass,
-            type: 'GET',
-            success: function (data) {
-              $('form.login input.username').addClass("hide");
-              $('form.login input[type="password"]').addClass("hide");
-              $('form.login button.login').removeClass("login");
-              $('form.login button').addClass("userInfo");
-              $('form.login button.userInfo').text("@"+data.userName);              
-            }
+      } else {
+          $.ajax({
+              url: '/app/Users/' + user + '/' + pass,
+              type: 'GET',
+              success: function (data) {
+                $('form.login input.username').addClass("hide");
+                $('form.login input[type="password"]').addClass("hide");
+                $('form.login button.login').removeClass("login");
+                $('form.login button').addClass("userInfo");
+                $('form.login button.userInfo').text("@"+data.userName);
+              }
           });
         }
       return false;
     }
 
-    function newUserModalShow(){
+    function newUserModalShow() {
       $('div.newUser').modal('show');
     }
 
-    function readyToCharge(){
+    function readyToCharge() {
       //verify that the user is loged and if so redirect to ReadyToCharge.html.
     }
 
-    function constructingModalShow(){
+    function constructingModalShow() {
       $('div.constructing').modal("show");
     }
 
 });
 
-    function postNewUser(){
-       var newUser = {
-          userName: $('form.newUser input.userName').val(),
-          userLastName: $('form.newUser input.userLastName').val(),
-          userId: $('form.newUser input.userId').val(),
-          userPass: $.md5($('form.newUser input.userPass').val()),
-          userAge: $('form.newUser input.userAge').val(),
-          userSex: $('form.newUser select.userSex').val()
-           };
+    function postNewUser() {
+      var user = {
+          id: $('form.newUser input[type="email"]').val(),
+          name: $('form.newUser input:eq(0)').val(),
+          last_name: $('form.newUser input:eq(1)').val(),
+          pass: $.md5($('form.newUser input[type="password"]').val()),
+          age: $('form.newUser input[type="number"]').val(),
+          sex: $('form.newUser select').val()
+      };
 
        $.ajax({
           url: '/app/Users',
           type: 'POST',
-          data: JSON.stringify(newUser),
+          data: JSON.stringify(user),
           contentType: 'application/json; charset=utf-8',
           async: false,
-          success: function(data,textStatus,jqXHR) {
+          success: function(data, textStatus, jqXHR) {
             alert(jqXHR.responseText);
             $('div.newUser').modal('hide');
             $('form.newUser')[0].reset();
           },
-          error: function(jqXHR,textStatus,erroThrown){
+          error: function(jqXHR, textStatus, erroThrown){
             alert(jqXHR.responseText);
             $('div.newUser').modal('hide');
             $('form.newUser')[0].reset();
           }
         });
- 
-      }
+    }
